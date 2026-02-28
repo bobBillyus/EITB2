9d914adbcbf0393890494c9341346fea77b6851a
bobBillyus <aryand4120@gmail.com>
2026-02-28 09:22:13 -0500

    adding graph

--- app.py
+++ app.py
@@ -7,14 +7,37 @@ import wikipedia
 from flask import Flask, render_template, request
 
 #This is the library for the graph visual
-import plotly.graph_objects as go
-import networkx as nx
+from dash import Dash, html
+import dash_cytoscape as cyto
 
+# Initialize Wikipedia API
 user = wikipediaapi.Wikipedia(user_agent='EITB2 (aryand4120@gmail.com)', language='en')
 
-app = Flask(__name__)
+# 1. Setup Flask first
+server = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+# 2. Setup Dash and attach it to the Flask server
+# The graph will be viewable at /graph/
+app = Dash(__name__, server=server, url_base_pathname='/graph/')
+
+# 3. Define the Dash Layout
+app.layout = html.Div([
+    html.P("Dash Cytoscape:"),
+    cyto.Cytoscape(
+        id='cytoscape',
+        elements=[
+            {'data': {'id': 'ca', 'label': 'Canada'}},
+            {'data': {'id': 'on', 'label': 'Ontario'}},
+            {'data': {'id': 'qc', 'label': 'Quebec'}},
+            {'data': {'id': 'e1', 'source': 'ca', 'target': 'on'}},
+            {'data': {'id': 'e2', 'source': 'ca', 'target': 'qc'}}
+        ],
+        layout={'name': 'breadthfirst'},
+        style={'width': '100%', 'height': '500px'}
+    )
+])
+
+@server.route('/', methods=['GET', 'POST'])
 def home():
     result = None
     suggestions = None
@@ -24,14 +47,18 @@ def home():
     if request.method == 'POST':
         user_query = request.form.get('wiki_page')
 
-    if user_query:
-        page = user.page(user_query)
-        if page.exists() == True:
-            url = page.fullurl
-            print(url)
-            message = "Page found!"
+        if user_query:
+            page = user.page(user_query)
+            if page.exists() == True:
+                url = page.fullurl
+                print(url)
+                message = "Page found!"
+            else:
+                message = "Page not found"
+                suggestions = wikipedia.search(str(user_query))
 
     return render_template('index.html', message=message, url=url, suggestions=suggestions)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Use server.run to start the combined Flask/Dash app
+    server.run(debug=True)
