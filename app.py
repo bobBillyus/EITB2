af349af6c8ed0db55c71675464fe936ca5fbcbd2
bobBillyus <aryand4120@gmail.com>
2026-03-01 20:51:26 -0500

    better search options

--- app.py
+++ app.py
@@ -30,9 +30,9 @@ def live_search():
     data = request.get_json()
     query = data.get("query", "")
 
-    if len(query) >= 2:
-        search_options = wikipedia.suggest(query) 
-    return jsonify(search_options[:5])
+    if len(query) >= 1:
+        search_options = wikipedia.search(query) 
+    return jsonify(search_options[:3])
 
 #Setup Dash
 app = Dash(__name__, server=server, url_base_pathname='/graph/')
